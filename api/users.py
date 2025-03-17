9acb358c4d97eb7729065003269e3e030b9f4d2b
Wendri23 <plenevauxadrien@gmail.com>
2025-03-17 10:15:00 +0100

    okok

--- api/users.py
+++ api/users.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, HTTPException, Request
-from config.db import add_user, get_user
+from fastapi import APIRouter, HTTPException, Request, Depends
+import jwt
+from datetime import datetime, timedelta
+import config.db as db
 
 UserRouter = APIRouter()
 
 # route pour creer un user
 @UserRouter.post("/")
-async def add_users(request: Request, username: str, email: str, password: str):
+async def add_user(request: Request, username: str, email: str, password: str):
     # pour le moment les info users sont en params de la fonction mais a terme ils seront rensegner dans un formulaire et envoyer auformat json 
     """Récupérer les données JSON envoyées dans le corps de la requête
     data = await request.json()
@@ -20,15 +22,15 @@ async def add_users(request: Request, username: str, email: str, password: str):
         )
     
     try:
-        add_user(username, email, password)
+        db.add_user(username, email, password)
         return {"message": "Utilisateur ajouté avec succès"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Erreur lors de l'ajout de l'utilisateur : {e}")
 
 # route pour get un user
 @UserRouter.get("/{user_id}")
-async def get_users(user_id: int):
-    user = get_user(user_id)
+async def get_user(user_id: int):
+    user = db.get_user(user_id)
     if not user:
       raise HTTPException(status_code=404, detail="Utilisateur non trouvé")
     
@@ -39,3 +41,8 @@ async def get_users(user_id: int):
             "email": user[2]
         }
     }
+
+# route de connexion utilisateur
+@UserRouter.post("/login")
+async def login_user(username: str, password: str, role: str):
+    
